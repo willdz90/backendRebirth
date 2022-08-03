7dcb2b6e18775f34e0260a211089c5764f0c11c1
WillDz90 <will.diazor@gmail.com>
2022-08-02 22:20:37 -0500

    Act de credenciales

--- src/db.js
+++ src/db.js
@@ -3,10 +3,12 @@ pg.defaults.ssl = true;
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/d6c44i46f8s9jc`,{
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/d6c44i46f8s9jc`,
+  `${DATABASE_URL}`,
+  {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions : {

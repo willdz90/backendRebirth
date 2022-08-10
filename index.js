b6bbecc9bae812d4fb62350930652f67bdd6afb7
WillDz90 <will.diazor@gmail.com>
2022-08-10 11:23:10 -0500

    Act

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { getPets } = require("./src/tools/preCharge.js");
 const { getAdmin } = require("./src/tools/preChargeAdmin.js");
 const { PORT } = process.env;
 
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   await getAdmin();
   await getPets();
   server.listen(PORT, () => {
